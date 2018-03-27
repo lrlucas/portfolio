01f4eb6f31b5bea2b93c00d11fc1a32ca8c30db7
lrlucas <lucas.suarez.utepsa@gmail.com>
2018-03-26 23:51:16 -0400

    Primer commit template listo para usarse

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component,OnInit  } from '@angular/core';
+//Se declaran las variables que jquery va usar
+declare var jquery:any;
+declare var $ :any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(){
+
+  }
+
+  ngOnInit(){  
+       
+    // Smooth scrolling using jQuery easing
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: (target.offset().top - 48)
+          }, 1000, "easeInOutExpo");
+          return false;
+        }
+      }
+    });
+  }
   title = 'app';
 }
