babd810fcad4213272a70b532739f3610308de02
lrlucas <lucas.suarez.utepsa@gmail.com>
2018-03-27 23:28:36 -0400

    Modificado cierre de menu cuando se hace scroll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -28,6 +28,9 @@ export class AppComponent implements OnInit {
         }
       }
     });
+     // Closes responsive menu when a scroll trigger link is clicked
+     $('.js-scroll-trigger').click(function() {
+      $('.navbar-collapse').collapse('hide');
+    });
   }
-  title = 'app';
 }
